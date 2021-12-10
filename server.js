8ff131a0387f8a74d2b87f9e09c9cb2037b67548
Daniel Bravo <daniel.brv@hotmail.cl>
2021-12-10 18:58:20 -0300

    Se agrega nodemon

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser'); 
 
-app.use('/', function (req, res) {
-    res.send('Oli');
+const router = express.Router();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(router);
+
+router.get('/message', function(req, res) {
+    res.send('Lista de mensaje');
 })
+
+router.delete('/message', function(req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('mensaje '+ req.body.text +' añadido');
+})
+
+// app.use('/', function (req, res) {
+//     res.send('Oli :D');
+// })
 
 app.listen(3000);
 console.log('Server running on port 3000');
