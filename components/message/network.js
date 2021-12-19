c6a596040f6f6a8b29c872c28da9ecbe9cc95f85
Daniel Bravo <daniel.brv@hotmail.cl>
2021-12-19 12:09:34 -0300

    Se agrega el controller de components/message

--- components/message/network.js
+++ components/message/network.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const response = require('../../network/response');
-
+const controller = require('./controller');
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -13,12 +13,13 @@ router.get('/', function(req, res) {
 })
 
 router.post('/', function(req, res) {
-    console.log(req.query);
-    if (req.query.error == "ok"){
-        response.error(req, res, 'Error inesperado', 500, 'es solo una simulación de los errores');
-    } else {
-        response.success(req, res, 'Creado correctamente', 201);
+    controller.addMessage(req.body.user, req.body.message)
+     .then((fullMessage) => {
+        response.success(req, res, fullMessage, 201);
     }
+    ).catch(e => {
+        response.error(req, res, 'Error al crear el mensaje', 400, 'Error en el controlador');
+    })
 })
 
 module.exports = router;
